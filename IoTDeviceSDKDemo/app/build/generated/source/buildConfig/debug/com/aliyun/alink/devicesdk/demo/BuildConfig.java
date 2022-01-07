933e29aaea00907658ff9f7828f9460e1c212664
Pick-Ocean <987370041@qq.com>
2022-01-07 13:07:09 +0800

    first commit

---
+++ IoTDeviceSDKDemo/app/build/generated/source/buildConfig/debug/com/aliyun/alink/devicesdk/demo/BuildConfig.java
@@ -0,0 +1,12 @@
+/**
+ * Automatically generated file. DO NOT MODIFY
+ */
+package com.aliyun.alink.devicesdk.demo;
+
+public final class BuildConfig {
+  public static final boolean DEBUG = Boolean.parseBoolean("true");
+  public static final String APPLICATION_ID = "com.aliyun.alink.devicesdk.demo";
+  public static final String BUILD_TYPE = "debug";
+  public static final int VERSION_CODE = 2;
+  public static final String VERSION_NAME = "android-9";
+}
